0d00a37adf246c93d8c7802315f62ef18cd26683
Joern Muehlencord <joern.muehlencord@vitroconnect.de>
2023-01-18 11:32:18 +0100

    added test application for joinfaces / adminfaces / JDK17

---
+++ joinfaces-adminfaces-jdk17/src/main/java/de/muehlencord/test/Joinfaces17Application.java
@@ -0,0 +1,13 @@
+package de.muehlencord.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Joinfaces17Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Joinfaces17Application.class, args);
+  }
+
+}
