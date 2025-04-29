7b6a9ad20f44a81760c72c3fb798e48053f81308
jomu78 <dev@muehlencord.de>
2025-04-29 15:32:09 +0200

    added caused by / fixed by

--- joinfaces-aot-inaccessible/src/main/java/de/muehlencord/test/DemoApplication.java
+++ joinfaces-aot-inaccessible/src/main/java/de/muehlencord/test/DemoApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Demonstrate AOT problems related SpringFramework 6.2.6
+ * Caused by: https://github.com/spring-projects/spring-framework/issues/34677 (6.2.6)
+ * Fixed by: https://github.com/spring-projects/spring-framework/commit/7f2c1f447f62207aa6eb1f42d4f8f07434bd8913 (6.2.7)
+ * See also: https://github.com/spring-projects/spring-framework/issues/34824
  *
  * @author Joern Muehlencord, 2025-04-29
  * @since 1.0.0
